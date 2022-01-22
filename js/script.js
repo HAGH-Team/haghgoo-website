ac1d5c44fbaa12c58939abbf3281a14b8aa43f53
hesawmgh1098 <72912744+hesawmgh1098@users.noreply.github.com>
2022-01-22 19:17:02 +0330

    haghgoo.tk

--- js/script.js
+++ js/script.js
@@ -30,3 +30,12 @@ window.onload = () => {
 }
 
 //
+// commands search script
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myCmd button").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
